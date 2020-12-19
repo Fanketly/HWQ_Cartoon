f4b6c3c4ca502ebb480c3849628dae39785ec579
fanketly <872023319@qq.com>
2020-12-19 22:45:31 +0800

    构建代码基础功能（笔记本代码）

---
+++ app/src/main/java/com/example/classification/SpeciesFragment.kt
@@ -0,0 +1,14 @@
+package com.example.classification
+
+import android.os.Bundle
+import com.example.base.BaseFragment
+import com.example.hwq_cartoon.R
+import com.example.hwq_cartoon.databinding.FragmentSpeciesBinding
+
+
+class SpeciesFragment : BaseFragment<FragmentSpeciesBinding>(R.layout.fragment_species) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/classification/SpeciesViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.classification
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Android Studio.
+ * User: HuangWeiQiang
+ * Date: 2020/12/19
+ * Time: 22:42
+ */
+class SpeciesViewModel :ViewModel(){
+
+
+}
